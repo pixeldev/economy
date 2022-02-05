3d1f20241ac2fb99a62fc50fef2a571f4af0caa2
pixeldev <pixeldev1729@gmail.com>
2022-02-05 15:12:32 -0600

    refactor(trew-dist): use a class and extends from AbstractModule

--- trew-dist/src/main/java/net/cosmogrp/economy/trew/EconomyModule.java
+++ trew-dist/src/main/java/net/cosmogrp/economy/trew/EconomyModule.java
@@ -1,15 +1,15 @@
 package net.cosmogrp.economy.trew;
 
-import me.yushust.inject.Binder;
+import me.yushust.inject.AbstractModule;
 import me.yushust.inject.key.TypeReference;
 import me.yushust.inject.scope.Scopes;
 import net.cosmogrp.economy.EconomyHandler;
 import net.cosmogrp.economy.context.TransactionContext;
 import net.cosmogrp.economy.transaction.executor.TransactionExecutor;
 
-public interface EconomyModule extends Binder {
+public class EconomyModule extends AbstractModule {
 
-    default void bindEconomyHandler(
+    public void bindEconomyHandler(
             String name,
             Class<? extends EconomyHandler> handler
     ) {

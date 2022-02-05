accd36666474633ff7ee7530445789a829f375b7
Angel Miranda <pixeldev1729@gmail.com>
2022-02-05 11:28:27 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenLocal()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+}
+
+tasks {
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+    }
+
+    processResources {
+        filesMatching("**/*.yml") {
+            filter<org.apache.tools.ant.filters.ReplaceTokens>(
+                "tokens" to mapOf("version" to project.version)
+            )
+        }
+    }
+}
